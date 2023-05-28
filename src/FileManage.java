6295a444f00b28f00e7fef55643f95aecb213e79
thangdangsad <21011631@st.phenikaa-uni.edu.vn>
2023-05-28 21:03:01 +0700

    update

--- src/FileManage.java
+++ src/FileManage.java
@@ -2,7 +2,22 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import javafx.scene.control.Alert;
+import org.apache.commons.io.FileUtils;
 public class FileManage {
+
+    public void showNotification(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Thông báo");
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    public void showError(String errorMessage) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Lỗi");
+        alert.setContentText(errorMessage);
+        alert.showAndWait();
+    }
     public void copyFile(File source, File dest) throws IOException {
         FileUtils.copyFile(source, dest);
         showNotification("File copied successfully!");
@@ -16,6 +31,8 @@ public class FileManage {
             showError(e.getMessage());
         }
     }
+
+
 
     public void pasteFile(File source, File dest) throws IOException {
         FileUtils.copyFile(source, dest);
